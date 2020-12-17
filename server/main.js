6c3bd8f995bc4c20927c508d97c3f958b62fc0b9
polunzh <polunzh@gmail.com>
2020-12-17 19:37:50 +0800

    fix the bug running in abtnode

--- server/main.js
+++ server/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+process.env.ROOT_URL = 'http://127.0.0.1:3000';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (process.env.BLOCKLET_PORT) {
+    process.env.PORT = process.env.BLOCKLET_PORT;
+  }
+
+  WebAppInternals.setBundledJsCssPrefix(`/${process.env.BLOCKLET_DID || ''}`);
 });
