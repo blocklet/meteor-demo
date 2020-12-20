1d89f63a5be3b172d5669f45edeb7a9edbea4eb0
wangshijun <wangshijun2010@gmail.com>
2020-12-21 07:09:24 +0800

    fix: github actions ci

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,11 +3,10 @@ set -e
 VERSION=$(cat version | awk '{$1=$1;print}')
 echo "publish version ${VERSION}"
 
-git config --local user.name "wangshijun"
-git config --local user.email "wangshijun2010@gmail.com"
+git config --local user.name "bot"
+git config --local user.email "bot@arcblock.io"
 
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
-sudo npm install -g @abtnode/cli
 
 echo "publishing to github releases..."
 npm run bundle
